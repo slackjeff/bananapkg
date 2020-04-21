d6c5df1fde840eaee9ace14fe8c2cdc2bf2d6cf3
Hugo Torres <hugotorres@protonmail.com>
2020-04-21 01:45:23 -0300

    Modificado organização do Header e adicionado variaveis de cores.

--- install.sh
+++ install.sh
@@ -1,35 +1,48 @@
 #!/usr/bin/env bash
-#=====================HEADER=========================================|
-#AUTOR
-# Jefferson 'slackjeff' Rocha <root@slackjeff.com.br>
-#
-#INSTALAÇÃO DO BANANA
-#
-#LICENÇA
-#  MIT
-#
-# Instalador para o banana
-#
-#CHANGELOG
-# (20/02/2019) - Jefferson Rocha
-#  - Adicionado suporte ao manual
-#  - Inserido Cabeçalho no script
-#  - Se falhar qualquer coisa agora sai =)
-#====================================================================|
 
-#================== VARIAVEIS
+######################################################################
+# Autor     :   Jefferson 'slackjeff' Rocha <root@slackjeff.com.br>  #
+# Licença   :   MIT                                                  #
+# Descrição :   Instalador para o banana                             #
+#                                                                    #
+# Changelog :   (21/04/2020) - Hugo Torres                           #
+#                   - Organizado o cabeçalho e HEADER                #
+#                   - Adicionado variaveis para cores                #
+#                   - Modificado mensagem do final da instalação     #
+#               (20/02/2019) - Jefferson Rocha                       #
+#                   - Adicionado suporte ao manual                   #
+#                   - Inserido Cabeçalho no script                   #
+#                   - Se falhar qualquer coisa agora sai =)          #
+######################################################################
+
+# ====================================================================
+#                           VARIAVEIS
+# ====================================================================
+
 prg='banana'
 DESTDIR=''
 
-#================== TESTES
-[[ "$UID" -ne "0" ]] && { echo "Only root."; exit 1 ;}
+# Variaveis de cores
+RED='\033[31;1m'
+YELLOW_BLINK='\033[33;5;1m'
+END='\033[m'
+
+# ====================================================================
+#                            TESTES
+# ====================================================================
+
+[[ "$UID" -ne "0" ]] && { echo -e "\nOnly ${RED}root${END}.\n"; exit 1 ;}
 
 # Cabeçalho simples
-echo "###################################################"
-echo "   Banana Install, bugs? root@slackjeff.com.br     "
-echo -e "###################################################\n"
+echo -e "
+###################################################
+#   Banana Install, bugs? root@slackjeff.com.br   #
+###################################################\n"
 
-#================== INICIO
+# ====================================================================
+#                            INICIO
+# ====================================================================
+
 # Analisa parametros
 for param in "$@"; do
     shift
@@ -46,6 +59,7 @@ echo -e "\nPermission and Copy archives\n"
 install -vDm755 -t "${DESTDIR}/sbin/" "$prg" || exit 1
 install -vDm644 -t "${DESTDIR}/usr/share/man/pt_BR/man8/" 'banana.8' || exit 1
 install -vDm644 -t "${DESTDIR}/usr/libexec/banana/" {core,help}'.sh' || exit 1
+
 # Verifica se arquivo de configuração existe para cria-lo ou .new
 if [[ -e "${DESTDIR}/etc/banana/${prg}.conf" ]]; then
     cmp -s "${prg}.conf" "${DESTDIR}/etc/banana/${prg}.conf" ||
@@ -57,4 +71,10 @@ fi
 # Criando diretórios se não existirem
 mkdir -vp "${DESTDIR}/var/lib/banana/"{list,desc,remove}
 
-echo -e "\nFINNALY! WORK NOW, call banana"
+# Mensagem apresentada apos finalizar a instalação.
+echo -e "
+                           +-----------------------+
+                           |     !!! FINNALY !!!   |
+                           | Work now, call ${YELLOW_BLINK}banana${END} |
+                           +-----------------------+
+"
